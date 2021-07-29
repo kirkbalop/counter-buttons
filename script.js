9330599555abb63965a7de1fceb529136d737bbf
Kirk <kirkbalop@gmail.com>
2021-07-29 14:58:14 -0700

    custom styling

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const decrement = document
     .querySelector('#decrement')
     .addEventListener('click', minusOne)
 const counter = document.querySelector('#counter')
+const counterContain = document.querySelector('.main-container')
 let count = 0
 
 // Functions
@@ -15,8 +16,10 @@ function plusOne() {
     counter.textContent = count
     if(count > 0){
         counter.style.color = 'green'
+        counterContain.style.boxShadow = '5px 10px green'
     } else if (count === 0){
         counter.style.color = 'black'
+        counterContain.style.boxShadow = '5px 10px black'
     }
 }
 
@@ -26,7 +29,9 @@ function minusOne(){
     counter.textContent = count
     if(count < 0){
         counter.style.color = 'red'
+        counterContain.style.boxShadow = '5px 10px red'
     } else if (count === 0){
         counter.style.color = 'black'
+        counterContain.style.boxShadow = '5px 10px black'
     }
 }
