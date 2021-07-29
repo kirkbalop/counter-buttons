3ec236fd541e514cd4cd9caac1f40998839c7d2d
Kirk <kirkbalop@gmail.com>
2021-07-29 14:42:26 -0700

    colorized counter based on count

--- script.js
+++ script.js
@@ -8,16 +8,25 @@ const decrement = document
 const counter = document.querySelector('#counter')
 let count = 0
 
-
 // Functions
 function plusOne() {
     count++
     console.log('PLUS')
     counter.textContent = count
+    if(count > 0){
+        counter.style.color = 'green'
+    } else if (count === 0){
+        counter.style.color = 'black'
+    }
 }
 
 function minusOne(){
     count--
     console.log('MINUS')
     counter.textContent = count
+    if(count < 0){
+        counter.style.color = 'red'
+    } else if (count === 0){
+        counter.style.color = 'black'
+    }
 }
